b2bfad2535187aa2f8e7d2284e08d6284823f334
Tran Quang Tinh <quangtinhtran97@gmail.com>
2021-03-12 13:03:12 +0700

    First commit index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// include composer autoload
+require 'vendor/autoload.php';
+
+//import the Intervention Image Manager Class
+use Intervention\Image\ImageManagerStatic as Image;
+
+// and you are ready to go...
+$image = Image::make('img/Untitled.png')->resize(1024, 768)->save('img/Untitled2.png',100);
+
+echo '<img src="img/Untitled2.png" alt="" />';
